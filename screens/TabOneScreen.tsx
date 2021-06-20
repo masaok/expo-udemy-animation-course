e116cb5aeb8730de7e85776281ce6e3161c94b7d
Masao Kitamura <masaok@gmail.com>
2021-06-19 18:18:33 -0700

    Opacity and translation animations work

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,27 +1,7 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { Animated, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo'
 import { Text, View } from '../components/Themed'
-
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <TouchableWithoutFeedback onPress={() => console.log('PRESSED')}>
-        <Animated.View>
-          <Text>HELLO</Text>
-        </Animated.View>
-      </TouchableWithoutFeedback>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  )
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -29,13 +9,78 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
+
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'tomato',
+  },
 })
+
+export default function TabOneScreen() {
+  const [animation, setAnimation] = useState(new Animated.Value(1))
+  const [translation, setTranslation] = useState(new Animated.Value(0))
+
+  const animatedStyles = {
+    opacity: animation,
+  }
+
+  const startAnimation = () => {
+    Animated.timing(animation, {
+      toValue: 0,
+      duration: 350,
+      useNativeDriver: true,
+    }).start(() => {
+      console.log('CALLBACK AFTER ANIMATION')
+      Animated.timing(animation, {
+        toValue: 1,
+        duration: 350,
+        useNativeDriver: true,
+      }).start()
+    })
+  }
+
+  const translatedStyles = {
+    transform: [
+      {
+        translateY: translation,
+      },
+    ],
+  }
+
+  const startTranslation = () => {
+    Animated.timing(translation, {
+      toValue: 300,
+      duration: 1500,
+      useNativeDriver: true,
+    }).start(() => {
+      translation.setValue(0)
+    })
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={startAnimation}>
+        <Animated.View style={[styles.box, animatedStyles]}>
+          <Text>OPACITY</Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={startTranslation}>
+        <Animated.View style={[styles.box, translatedStyles]}>
+          <Text>TRANSLATION</Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    </View>
+  )
+}
