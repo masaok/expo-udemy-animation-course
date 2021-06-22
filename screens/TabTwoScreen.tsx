df7b6f7b1a31eccf16efe91094727d983e32f194
Masao Kitamura <masaok@gmail.com>
2021-06-22 02:42:44 -0700

    Color Interpolation works

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,18 +1,8 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react'
+import { Animated, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
-}
+import EditScreenInfo from '../components/EditScreenInfo'
+import { Text, View } from '../components/Themed'
 
 const styles = StyleSheet.create({
   container: {
@@ -20,13 +10,69 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
+
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
-});
+
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'tomato',
+  },
+})
+
+export default function TabTwoScreen() {
+  const [animation, setAnimation] = useState(new Animated.Value(0))
+
+  const startAnimation = () => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 1500,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 1500,
+        useNativeDriver: false,
+      }).start()
+    })
+  }
+
+  const boxInterpolation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['rgb(255,99,71)', 'rgb(99,71,255)'],
+    // outputRange: ['#FF0000', '#00FF00'],
+  })
+
+  const colorInterpolation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['rgb(99,71,255)', 'rgb(255,99,71)'],
+    // outputRange: ['#0000FF', '#FFFF00'],
+  })
+
+  const boxAnimatedStyle = {
+    backgroundColor: boxInterpolation,
+  }
+
+  const textAnimatedStyle = {
+    color: colorInterpolation,
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={startAnimation}>
+        <Animated.View style={[styles.box, boxAnimatedStyle]}>
+          <Animated.Text style={textAnimatedStyle}>Hello Animation</Animated.Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    </View>
+  )
+}
